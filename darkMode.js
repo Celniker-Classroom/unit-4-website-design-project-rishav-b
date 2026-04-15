661387d5d974d310ea8869c45a6a9a5a178e121c
rishav-b <rishav_b@outlook.com>
2026-04-15 11:06:43 -0700

    fixed dark mode

--- darkMode.js
+++ darkMode.js
@@ -1,5 +1,6 @@
 function dark() {  
-    const baseClasses = ["content", "whitespace", "hiking-header", "stars","header"];
+    const baseClasses = ["content", "whitespace", "hiking-header", "stars","header","map"];
+    const baseIds = ["nav-bar"];
     
     baseClasses.forEach((className) => {
         // Select elements that have either class
@@ -13,6 +14,23 @@ function dark() {
             }
         });
     });
+
+    baseIds.forEach((baseId) => {
+        const baseDiv = document.getElementById(baseId);
+        if (baseDiv.style.backgroundColor === "rgb(26, 22, 17)") {
+            baseDiv.style.backgroundColor = "rgb(95, 79, 60)";
+        } else {
+            baseDiv.style.backgroundColor = "rgb(26, 22, 17)";
+        }
+    })
+
+    const darkButton = document.getElementById("dark-image");
+    if (darkButton.getAttribute("src").includes("dark_mode")) {
+        darkButton.setAttribute("src", "images/light_mode.svg");
+    } else {
+        darkButton.setAttribute("src", "images/dark_mode.svg");
+    }
+
 }
 
 document.getElementById("dark").addEventListener("click", dark);
